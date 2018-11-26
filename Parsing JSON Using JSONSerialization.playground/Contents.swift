0dce356a4219e1a493550044fc9d42a709cf6fa9
masato <masato@M-mbp.local>
2018-11-26 20:04:50 +0800

    initial commit

---
+++ Parsing JSON Using JSONSerialization.playground/Contents.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+struct Customer {
+
+    var firstName :String
+    var lastName :String
+    var age :Int
+}
+
+
+extension Customer {
+
+    init?(dictionary :[String:Any]) {
+
+        guard let firstName = dictionary["firstName"] as? String,
+              let lastName = dictionary["lastName"] as? String,
+            let age = dictionary["age"] as? Int else {
+                return nil
+        }
+
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+    }
+
+}
+    
+
+
+let json = """
+
+{
+"firstName" : "John",
+"lastName"  : "Doe",
+"age"  : 34
+}
+
+""".data(using: .utf8)!
+
+
+if let dictionary = try! JSONSerialization.jsonObject(with: json, options: .allowFragments) as? [String:Any] {
+
+    if let customer = Customer(dictionary: dictionary) {
+        print(customer)
+    }
+}
+
+print("Hello World")
