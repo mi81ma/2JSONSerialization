641b41d8d21223d9178e3c0e583ee89419697b5b
mi81ma <mi81ma@gmail.com>
2018-12-05 15:42:45 +0800

    2nd time Practice

--- Parsing JSON Using JSONSerialization.playground/Contents.swift
+++ Parsing JSON Using JSONSerialization.playground/Contents.swift
@@ -1,19 +1,67 @@
 import UIKit
 
-struct Customer {
+//struct Customer {
+//
+//    var firstName :String
+//    var lastName :String
+//    var age :Int
+//}
+//
+//
+//extension Customer {
+//
+//    init?(dictionary :[String:Any]) {
+//
+//        guard let firstName = dictionary["firstName"] as? String,
+//              let lastName = dictionary["lastName"] as? String,
+//            let age = dictionary["age"] as? Int else {
+//                return nil
+//        }
+//
+//        self.firstName = firstName
+//        self.lastName = lastName
+//        self.age = age
+//    }
+//
+//}
+//
+//
+//
+//let json = """
+//
+//{
+//"firstName" : "John",
+//"lastName"  : "Doe",
+//"age"  : 34
+//}
+//
+//""".data(using: .utf8)!
 
+//
+//if let dictionary = try! JSONSerialization.jsonObject(with: json, options: .allowFragments) as? [String:Any] {
+//
+//    if let customer = Customer(dictionary: dictionary) {
+//        print(customer)
+//    }
+//}
+//
+//print("Hello World")
+//
+
+
+/// :******************* New Note ***************************
+
+
+struct Customer {
     var firstName :String
     var lastName :String
     var age :Int
 }
 
-
 extension Customer {
-
     init?(dictionary :[String:Any]) {
-
         guard let firstName = dictionary["firstName"] as? String,
-              let lastName = dictionary["lastName"] as? String,
+        let lastName = dictionary["lastName"] as? String,
             let age = dictionary["age"] as? Int else {
                 return nil
         }
@@ -24,25 +72,22 @@ extension Customer {
     }
 
 }
-    
-
 
 let json = """
-
 {
-"firstName" : "John",
+"firstName": "John",
 "lastName"  : "Doe",
-"age"  : 34
+"age" : 34
 }
-
 """.data(using: .utf8)!
 
-
 if let dictionary = try! JSONSerialization.jsonObject(with: json, options: .allowFragments) as? [String:Any] {
-
     if let customer = Customer(dictionary: dictionary) {
         print(customer)
     }
+
 }
 
-print("Hello World")
+
+
+
